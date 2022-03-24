3fe87b606f210f0ab842bbd2c71bd6373a952f36
1kper <somprav59@gmail.com>
2022-03-24 07:14:42 +0530

    hashing is chnaged from level 3 (md5 hash ) to level 4 (bcrypt hash)

--- routes.js
+++ routes.js
@@ -2,8 +2,10 @@ require('dotenv').config();
 const express = require("express");
 const _ = require("lodash");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption")
-
+// const md5 = require("md5"):
+//
+const bcrypt = require("bcrypt");
+const saltRound =10;
 
 
 
@@ -26,7 +28,7 @@ const userSchema = new mongoose.Schema({
 
 
 
-userSchema.plugin(encrypt,{secret:process.env.SECRET, encryptedFields:["password"]});
+// userSchema.plugin(encrypt,{secret:process.env.SECRET, encryptedFields:["password"]});
 
 
 const User = new mongoose.model("User", userSchema);
@@ -65,12 +67,12 @@ const app = express();
 
 app.route("/")
   .get((req, res) => {
-           res.render("home");})
+           res.render("home");});
 
 ///////////////////////////////routes for /home path of url////////////////////////////////////
 app.route("/home")
   .get((req, res) => {
-           res.render("home");})
+           res.render("home");});
 
 
 
@@ -83,16 +85,22 @@ app.route("/register")
 
 
    .post((req,res)=>{
-        const newUserName=req.body.username;
-         const newPassword =req.body.password;
-         const newUser= new User({email:newUserName,password:newPassword}) ;
-         newUser.save((err)=>{
-           if(err){res.send(err)}
-           else{ res.render("secrets");}
-         })
+
+    bcrypt.hash(req.body.password,saltRound,function(err,hash){
+      const newUserName=req.body.username;
+       const newPassword =hash;
+       const newUser= new User({email:newUserName,password:newPassword}) ;
+       newUser.save((err)=>{
+         if(err){res.send(err)}
+         else{ res.render("secrets");}
+       })
+
+    })
 
 
-   })
+
+
+  });
    ///////////////////////////////routes for /login path of url////////////////////////////////////
    app.route("/login")
      .get((req, res) => {
@@ -107,11 +115,19 @@ app.route("/register")
 
               else{ if(foundUser)
                          {
-                           if(foundUser.password === enteredPassword){
-                                               res.render("secrets")
-                                              }
-                          else {res.render("login",{message:"Your password is incorrect please re-enter your password."})
-                               }
+
+                           bcrypt.compare(enteredPassword,foundUser.password,function(err,result){
+
+                             if(result === true){   res.render("secrets")}
+                             else {res.render("login",{message:"Your password is incorrect please re-enter your password."})
+                                  }
+
+                           }
+
+
+                         )
+
+
                           }
                     else{res.render("login",{message:" Please check your username is correct or You need to Register with us."})
                         }
@@ -124,7 +140,7 @@ app.route("/register")
                })
 
 
-      })
+      });
 
 ///////////////////////////////routes for /secrets path of url////////////////////////////////////
 // app.route("/secrets")
@@ -133,7 +149,7 @@ app.route("/register")
 //////////////////////////////routes for / submit path of url////////////////////////////////////
 app.route("/submit")
   .get((req, res) => {
-           res.render("submit");})
+           res.render("submit");});
 ///////////////////////////////routes for logout path of url////////////////////////////////////
 app.route("/logout")
   .get((req,res)=>{res.render("home")});
